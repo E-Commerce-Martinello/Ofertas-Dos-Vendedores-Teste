02524b3266d797738209509a249d2539006b1ad9
E-Commerce-Martinello <ecommerce.martinello@gmail.com>
2026-03-05 13:29:37 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,20 +1,23 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// ============================================
+// firebase-config.js - CONFIGURAÇÃO DO FIREBASE
+// ============================================
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCxcM4BgmGk62qIC7KbM_7nrjrMKwS_TOc",
-    authDomain: "ofertas-vendedores.firebaseapp.com",
-    databaseURL: "https://ofertas-vendedores-default-rtdb.firebaseio.com",
-    projectId: "ofertas-vendedores",
-    storageBucket: "ofertas-vendedores.firebasestorage.app",
-    messagingSenderId: "811095753027",
-    appId: "1:811095753027:web:155270bef73de3e28b2ff6"
-  };
+// Sua configuração do Firebase (colei aqui pra você!)
+const firebaseConfig = {
+  apiKey: "AIzaSyCxcM4BgmGk62qIC7KbM_7nrjrMKwS_TOc",
+  authDomain: "ofertas-vendedores.firebaseapp.com",
+  databaseURL: "https://ofertas-vendedores-default-rtdb.firebaseio.com",
+  projectId: "ofertas-vendedores",
+  storageBucket: "ofertas-vendedores.firebasestorage.app",
+  messagingSenderId: "811095753027",
+  appId: "1:811095753027:web:155270bef73de3e28b2ff6"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Conectar ao banco de dados
+const database = firebase.database();
+
+// Referência para nossas ofertas
+const ofertasRef = database.ref('ofertas');
