058151572233db9b7148c481c5da66a69d3669d0
E-Commerce-Martinello <ecommerce.martinello@gmail.com>
2026-03-05 11:24:38 -0400

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,20 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCxcM4BgmGk62qIC7KbM_7nrjrMKwS_TOc",
+    authDomain: "ofertas-vendedores.firebaseapp.com",
+    databaseURL: "https://ofertas-vendedores-default-rtdb.firebaseio.com",
+    projectId: "ofertas-vendedores",
+    storageBucket: "ofertas-vendedores.firebasestorage.app",
+    messagingSenderId: "811095753027",
+    appId: "1:811095753027:web:155270bef73de3e28b2ff6"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
